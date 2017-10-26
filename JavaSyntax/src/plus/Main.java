b8b414c21aaf2d76b559414cb9e0496228ed1994
Veselin Stoyanov <veselin1996@gmail.com>
2017-10-26 23:28:10 +0300

    Variables and plus operator

---
+++ JavaSyntax/src/plus/Main.java
@@ -0,0 +1,21 @@
+package plus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        double c = 1.1;
+        double d = 2.2;
+
+        String e = "Hello, ";
+        String f = "World!";
+
+        System.out.println(a + b);
+        System.out.println(c + d);
+        System.out.println(e + f);
+        System.out.println(a + c);
+        System.out.println(e + a);
+    }
+}
